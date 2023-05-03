e267b3af104d71ebca8762368928bb18ff61289a
Rafael Correa Alves <correa_rafael@sicredi.com.br>
2023-05-03 11:15:51 -0300

    Initial commit

---
+++ src/main/java/com/senac/bibliotecamaven/BibliotecaMaven.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.senac.bibliotecamaven;
+
+/**
+ *
+ * @author correa_rafael
+ */
+public class BibliotecaMaven {
+
+    public static void main(String[] args) {
+        MainFrame main = new MainFrame();
+        main.setVisible(true);
+        System.out.println("Hello World!");
+    }
+}
